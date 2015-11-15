67f408e924745f0b61c1ea425063cc9010fc1cde
Nickstreme <nicobravo1@hotmail.com>
2015-11-15 17:01:52 -0500

    Tested MoveTimeNico

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTimeNico.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTimeNico.java
@@ -1,8 +1,6 @@
 package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
 
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Nicolas Bravo on 10/30/15.
@@ -10,7 +8,7 @@ package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
 public class MoveTimeNico extends Telemetry9926{
 
     int move_state = 0;
-//    Servo Servo1;
+    Servo Servo1;
     double SM1_Position;
 
 
