8d6595e177c24f2b97b77ebb3ddeeb5f6477c8cf
Nickstreme <nicobravo1@hotmail.com>
2015-11-16 20:48:17 -0500

    Just an edit

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTimeServo.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTimeServo.java
@@ -9,18 +9,14 @@ public class MoveTimeServo extends Telemetry9926{
 
     int move_state = 0;
     Servo Servo1;
-    Servo Servo2;
     double SM1_Position;
-    double SM2_Position;
 
 
     @Override
     public void init() {
         Define_Hardware_Config_Names();
         Servo1 = hardwareMap.servo.get("SM1");
-        Servo2 = hardwareMap.servo.get("SM2");
         Servo1.setPosition(SM1_Position);
-        Servo2.setPosition(SM2_Position);
 
     }
 
@@ -48,9 +44,7 @@ public class MoveTimeServo extends Telemetry9926{
 
             case 2:
                 SM1_Position = 0.2;
-                SM2_Position = 0.2;
                 Set_Servo_position(SM1_Position);
-                Set_Servo_position(SM2_Position);
                 if (getRuntime() > 10)
                 {
                     move_state++;
@@ -59,9 +53,7 @@ public class MoveTimeServo extends Telemetry9926{
 
             case 3:
                 SM1_Position = 0.3;
-                SM2_Position = 0.3;
                 Set_Servo_position(SM1_Position);
-                Set_Servo_position(SM2_Position);
                 if (getRuntime() > 15){
                     move_state++;
                 }
@@ -73,8 +65,7 @@ public class MoveTimeServo extends Telemetry9926{
         UpdateTelemetry();
         telemetry.addData("11", "State: " + move_state);
         telemetry.addData("12", "Time: " + getRuntime());
-        telemetry.addData("13","Servo 1: " + SM1_Position);
-        telemetry.addData("14", "Servo 2: " + SM2_Position);
+        telemetry.addData("13", "Servo 1: " + SM1_Position);
     }
 
     @Override
