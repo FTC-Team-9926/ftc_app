7838efff9fe26154cb4f8fe0fef2207044cbdcf2
Ignacio Bravo <ibravo@hotmail.com>
2015-11-23 19:00:07 -0500

    Removed unnecesary files

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/AutoMove.java
@@ -0,0 +1,11 @@
+package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
+
+/**
+ * Created by ibravo on 11/23/15.
+ * Should contain the AutoBot for Team 9926
+ */
+public class AutoMove extends Telemetry9926 {
+
+
+
+}
