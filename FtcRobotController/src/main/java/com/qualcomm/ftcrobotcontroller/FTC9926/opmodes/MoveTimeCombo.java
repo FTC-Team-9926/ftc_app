61fcdc03a394c87924ecc9ea39f9a1aa7de9707f
Nickstreme <nicobravo1@hotmail.com>
2015-11-23 20:28:19 -0500

    Added the "time" double to keep up with getRuntime().

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTimeCombo.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTimeCombo.java
@@ -4,32 +4,29 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by Nicolas Bravo on 11/16/15.
- * This program is a combination of MoveTimeServo.java and MoveTimeMotor.java
- * It is for use in the autonomous section of an FTC match
+ * Created by nicolasbravo on 11/16/15.
  */
-
 public class MoveTimeCombo extends Telemetry9926{
-    /* Defines the actual motors
-     * Defines move_state as an integer with a value of 0 */
+
     DcMotor Motor1;
     DcMotor Motor2;
     Servo Servo1;
     int move_state = 0;
-    double SM1_Position;
+    double time = 0;
 
     @Override
     public void init() {
         Define_Hardware_Config_Names();
-        /* Defines the motor names */
         Motor1 = hardwareMap.dcMotor.get("M1");
         Motor2 = hardwareMap.dcMotor.get("M2");
+        Motor2.setDirection(DcMotor.Direction.REVERSE);
         Servo1 = hardwareMap.servo.get("SM1");
         Servo1.setPosition(SM1_Position);
     }
 
     @Override
     public void start() {
+
     }
 
     @Override public void loop()
@@ -37,67 +34,59 @@ public class MoveTimeCombo extends Telemetry9926{
         switch (move_state)
         {
             case 0:
-                /* FIRST CASE
-                 * Only the servo moves
-                 * Waits 5 seconds */
+                /* might need to reset motor */
                 SM1_Position = 0.2;
                 Set_Servo_position(SM1_Position);
                 move_state++;
+                time = getRuntime();
                 break;
+
             case 1:
-                /* SECOND CASE
-                 * Nothing moves
-                 * Resets the DC Motors */
-                if (getRuntime() > 5){
+                if ((getRuntime() - time) >= 5){
                     Motor1.setPower(0);
                     Motor2.setPower(0);
                     move_state++;
+                    time = getRuntime();
                 }
                 break;
+
             case 2:
-                /* THIRD CASE
-                 * Servo and DC Motors move
-                 * Only one of the DC Motors move
-                 * Waits 5 seconds, and then resets motors */
                 SM1_Position = 0.3;
                 Set_Servo_position(SM1_Position);
                 Motor1.setPower(1);
-                Motor2.setPower(0);
-                if (getRuntime() > 10)
+                Motor2.setPower(1);
+                if ((getRuntime() - time) >= 5)
                 {
                     Motor1.setPower(0);
                     Motor2.setPower(0);
                     move_state++;
+                    time = getRuntime();
                 }
                 break;
+
             case 3:
-                /* FOURTH CASE
-                 * Only DC Motors Move
-                 * Waits 5 seconds, and then resets motors */
                 Motor1.setPower(1);
-                Motor2.setPower(1);
-                if (getRuntime() > 15){
+                Motor2.setPower(0);
+                if ((getRuntime() - time) > 5){
                     Motor1.setPower(0);
                     Motor2.setPower(0);
                     move_state++;
+                    time = getRuntime();
                 }
                 break;
             default:
-                /* DEFAULT CASE
-                 * States the default
-                 * For use if move_state is greater than four */
                 break;
         }
 
         UpdateTelemetry();
-        /* TELEMETRY
-         * Displays data on phone */
         telemetry.addData("11", "State: " + move_state);
-        telemetry.addData("12", "Time: " + getRuntime());
-        telemetry.addData("13", "Servo: " + SM1_Position);
+        telemetry.addData("12", "Time (Total): " + getRuntime());
+        telemetry.addData("13", "Time (Task): " + (getRuntime() - time));
+        telemetry.addData("14", "Servo 1: " + SM1_Position);
     }
 
     @Override
     public void stop() {
+
     }
 }
