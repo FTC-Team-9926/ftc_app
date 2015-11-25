1c2e0a1b20e6a851ace1ce84612676c20b118527
vijay <rudraraju.vijay5@gmail.com>
2015-11-25 17:43:54 -0500

    Merge branch 'Ignacio' of https://github.com/FTC-Team-9926/ftc_app into vijay
    # Conflicts:
    #	FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTime.java
    #	FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/Auto_Vijay_Encoders.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/Auto_Vijay_Encoders.java
@@ -1,10 +1,14 @@
 package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
- *  Created by vijay on 11/20/2015.
+ * Created by vijay on 11/20/2015.
  */
 public class Auto_Vijay_Encoders extends HardwareMap9926{
 
+        DcMotor Motor1;
+        DcMotor Motor2;
 
 
 
