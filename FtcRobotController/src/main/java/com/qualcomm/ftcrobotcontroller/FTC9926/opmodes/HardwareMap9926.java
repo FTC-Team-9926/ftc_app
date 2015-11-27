2481ac601399dbafe4cd32f06349a05196e6911d
Ignacio Bravo <ibravo@hotmail.com>
2015-11-27 16:27:09 -0500

    Created Auto9926 for the Main Autonomous Program for 9926
    Moved hardware initialization to the HardwareMap9926 class

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/HardwareMap9926.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/HardwareMap9926.java
@@ -29,6 +29,10 @@ public class HardwareMap9926 extends OpMode {
     DcMotor Motor3;
     double SM1_Position;
     double Get_Servo_Position;
+    DcMotor Motor1;
+    DcMotor Motor2;
+    DcMotor Motor3;
+
 
     @Override
     public void init(){
@@ -94,6 +98,7 @@ public class HardwareMap9926 extends OpMode {
 //        v_servo_left_hand.setPosition(.2);
 
     } // PushBotManual::m_hand_position
+
     void Move (double Motor1power, double Motor2power){
         if (Motor1 != null)
         {
@@ -132,5 +137,64 @@ public class HardwareMap9926 extends OpMode {
         Motor1.setPower(0);
         Motor2.setPower(0);
         Motor3.setPower(0);
+    }
+
+    /*
+ * This method scales the joystick input so for low joystick values, the
+ * scaled value is less than linear.  This is to make it easier to drive
+ * the robot more precisely at slower speeds.
+ */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+
+    void MoveRobot (double Engine1, double Engine2)
+    {
+        {
+            if (Motor1 != null)
+            {
+                Motor1.setPower (Engine1);
+            }
+
+        } // set_drive_power
+            if (Motor2 != null)
+            {
+                Motor2.setPower (Engine2);
+            }
+    }
+    void MoveArm (double Engine3)
+    {
+        {
+            if (Motor3 != null)
+            {
+                Motor3.setPower (Engine3);
+            }
+
+        } // set_arm_power
     }
 }
