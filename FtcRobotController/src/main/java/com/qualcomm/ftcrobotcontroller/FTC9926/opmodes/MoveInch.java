2cd552d8da5a6390057f37ebd9321bbd68d98c7f
Nickstreme <nicobravo1@hotmail.com>
2015-11-27 17:48:32 -0500

    Added comments

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveInch.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveInch.java
@@ -34,14 +34,19 @@ public class MoveInch extends Telemetry9926 {
         switch (move_state)
         {
             case 0:
+                /* CASE ZERO
+                The "reset" of the program
+                 */
                 time = getRuntime();
                 move_state++;
                 break;
             case 1:
-                while ((getRuntime() - time) < (inch * 150)) {
-                    Move(1, 1);
-                }
-                if ((getRuntime() - time) >= (inch * 150)) {
+                /* CASE ONE
+                First moves for a foot
+                Then stops and prepares for the next case
+                 */
+                Move(1, 1);
+                if ((getRuntime() - time) >= (inch * 12)) {
                 /* If getRuntime() minus time is greater than or equal to inch * amount of inches */
                     Stop();
                     time = getRuntime();
@@ -49,49 +54,60 @@ public class MoveInch extends Telemetry9926 {
                 }
                 break;
             case 2:
-                while ((getRuntime() - time) < (inch * 75)) {
-                    Move(-1, -1);
-                }
-                if ((getRuntime() - time) >= (inch * 75)) {
+                /* SECOND CASE
+                First moves backwards for a foot
+                Then stops and prepares for the next case
+                 */
+                Move(-1, -1);
+                if ((getRuntime() - time) >= (inch * 12)) {
                     Stop();
                     time=getRuntime();
                     move_state++;
                 }
                 break;
             case 3:
-                while ((getRuntime() - time) < (inch * 25)) {
-                    Move(0.7, 1);
-                }
-                if((getRuntime() - time) >= (inch * 25)) {
+                /* THIRD CASE
+                First moves slightly to the right for a foot
+                Then stops and prepares for the next case
+                 */
+                Move(0.7, 1);
+                if((getRuntime() - time) >= (inch * 12)) {
                     Stop();
                     time = getRuntime();
                     move_state++;
                 }
                 break;
             case 4:
-                while ((getRuntime() - time) < 5) {
-                    Turn(1);
-                }
-                if ((getRuntime() - time) >= 5) {
+                /* FOURTH CASE
+                First tank turns to the right for three seconds
+                Then stops and prepares for the next case
+                 */
+                Turn(1);
+                if ((getRuntime() - time) >= 3) {
                     Stop();
                     time = getRuntime();
                     move_state++;
                 }
                 break;
             case 5:
-                while ((getRuntime() - time) < 5) {
-                    Turn(-1);
-                }
-                if ((getRuntime() - time) >= 5) {
+                /* FIFTH CASE
+                First tank turns to the left for three seconds
+                Then stops and prepares for the next case
+                 */
+                Turn(-1);
+                if ((getRuntime() - time) >= 3) {
                     Stop();
                     time = getRuntime();
                     move_state++;
                 }
                 break;
             case 6:
-                while ((getRuntime() - time) < 0.3) {
-                    Arm (1, 0.5);
-                }
+                /* SIXTH CASE
+                First moves the arm forwards for 0.3 seconds
+                While moving the servo 0.5
+                Then stops and prepares for the next case
+                 */
+                Arm(1, 0.5);
                 if ((getRuntime() - time) >= 0.3){
                     Stop();
                     time = getRuntime();
