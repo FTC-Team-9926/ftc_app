f52cd431323f41caf45e9ae8033240791c6a2716
MCENGINEER11 <jackhogan11@gmail.ocm>
2015-11-28 16:56:56 -0500

    Improved encoder program

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/Encoders.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/Encoders.java
@@ -4,12 +4,12 @@ package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
  * Created by jackhogan on 11/27/2015.
  */
 public class Encoders extends Telemetry9926{
-    @Override
+
     public void init(){}
 
-    @Override
     public void start(){
         reset_encoders();
+        run_using_encoders();
     }
 
     public void loop(){
@@ -19,6 +19,9 @@ public class Encoders extends Telemetry9926{
        else{
             MoveRobot(0,0);
         }
+        UpdateTelemetry();
+        telemetry.addData("Program","Runtime:"+ getRuntime());
+        telemetry.addData("Encoder","State:"+ left_encoder_count());
     }
 
     public void stop(){}
