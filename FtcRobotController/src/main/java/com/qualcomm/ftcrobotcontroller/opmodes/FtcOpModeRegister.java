f7bfda59d97238dc8e9c551f7c2518da8eecd357
Nickstreme <nicobravo1@hotmail.com>
2015-11-28 16:43:43 -0500

    Edits

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -1,20 +1,22 @@
-/* Copyright (c) 2014, 2015 Technologies Inc
-<<<<<<< HEAD
-=======
+/* Copyright (c) 2014, 2015 Qualcomm Technologies Inc
 
->>>>>>> MoveTime
 All rights reserved.
+
 Redistribution and use in source and binary forms, with or without modification,
 are permitted (subject to the limitations in the disclaimer below) provided that
 the following conditions are met:
+
 Redistributions of source code must retain the above copyright notice, this list
 of conditions and the following disclaimer.
+
 Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
-Neither the name of Technologies Inc nor the names of its contributors
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
 may be used to endorse or promote products derived from this software without
 specific prior written permission.
+
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
 LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -29,16 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.HardwareMap9926;
-import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.MoveForward;
-import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.MoveInch;
-//import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.MoveServo;
-//import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.MoveStick;
-import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.MoveTank;
-import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.MoveTimeCombo;
-import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.MoveTimeMotor;
-import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.MoveTimeServo;
-import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.TouchOp;
+import com.qualcomm.ftcrobotcontroller.FTC9926.opmodes.*;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
 
@@ -58,45 +51,26 @@ public class FtcOpModeRegister implements OpModeRegister {
     /*
      * register your op modes here.
      * The first parameter is the name of the op mode
-     * The second parameter is the op mode class property
+     * The second parameter is the op mode class property 
      *
      * If two or more op modes are registered with the same name, the app will display an error.
 <<<<<<< HEAD
-
     */
     manager.register("MoveForward", MoveForward.class);
 //    manager.register("Servo", MoveServo.class);
 //    manager.register("Stick", MoveStick.class);
     manager.register("Tank", MoveTank.class);
     manager.register("Touch", TouchOp.class);
-    manager.register("ServoTime", MoveTimeServo.class);
+    manager.register("Servo", MoveTimeServo.class);
     manager.register("Motor", MoveTimeMotor.class);
     manager.register("Combo", MoveTimeCombo.class);
-
     manager.register("NullOp", NullOp.class);
-
-    //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-//    manager.register("K9TeleOp", K9TeleOp.class);
-//    manager.register("K9Line", K9Line.class);
-//    manager.register ("PushBotAuto", PushBotAuto.class);
-//    manager.register ("PushBotManual", PushBotManual.class);
-
-//    manager.register("MoveForward", MoveForward.class);
-    manager.register("Inch", MoveInch.class);
-//    manager.register("Tank", MoveTank.class);
-//    manager.register("Combo", MoveTimeCombo.class);
-//    manager.register("Motor", MoveTimeMotor.class);
-//    manager.register("TimeServo", MoveTimeServo.class);
-//    manager.register("Touch", TouchOp.class);
-
-//    manager.register("NullOp", NullOp.class);
 
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register("K9Line", K9Line.class);
-    //manager.register ("PushBotAuto", PushBotAuto.class);
+//    manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
-
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
@@ -121,9 +95,11 @@ public class FtcOpModeRegister implements OpModeRegister {
 //    manager.register("K9IrSeeker", K9IrSeeker.class);
 //    manager.register("K9Line", K9Line.class);
 
+
     //manager.register ("PushBotManual1", PushBotManual1.class);
     //manager.register ("PushBotAutoSensors", PushBotAutoSensors.class);
     //manager.register ("PushBotIrEvent", PushBotIrEvent.class);
+
 
     //manager.register("PushBotManual", PushBotManual.class);
     //manager.register("PushBotAuto", PushBotAuto.class);
@@ -137,6 +113,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register ("PushBotOdsDetectEvent", PushBotOdsDetectEvent.class);
     //manager.register ("PushBotOdsFollowEvent", PushBotOdsFollowEvent.class);
     //manager.register ("PushBotTouchEvent", PushBotTouchEvent.class);
+
 
     //manager.register("PushBotDriveTouch", PushBotDriveTouch.java);
     //manager.register("PushBotIrSeek", PushBotIrSeek.java);
