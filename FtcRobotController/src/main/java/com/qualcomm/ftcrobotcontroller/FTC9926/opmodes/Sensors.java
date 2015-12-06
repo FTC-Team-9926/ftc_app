eb9a83cd4bd6c867726c42fc2f19e9f40431290a
MCENGINEER11 <jackhogan11@gmail.ocm>
2015-12-06 15:12:40 -0500

    Made sensors program

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/Sensors.java
@@ -0,0 +1,21 @@
+package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
+
+/**
+ * Created by jackhogan on 12/6/2015.
+ */
+public class Sensors extends HardwareMap9926{
+
+    @Override
+    public void init(){}
+
+    public void start(){
+        super.start();
+    }
+
+    public void loop(){
+        //color sensor
+    }
+
+    @Override
+    public void stop(){}
+}
