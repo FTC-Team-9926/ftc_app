07296b667af574f5de99dc6e77b6a4964e7dc769
vijay <rudraraju.vijay5@gmail.com>
2015-12-14 18:07:23 -0500

    Merge branches 'Auto_Vijay_Encoders', 'Nicolas' and 'master' of https://github.com/FTC-Team-9926/ftc_app into Auto_Vijay_Encoders
    # Conflicts:
    #	FtcRobotController/FtcRobotController.iml
    #	FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
    
    I made an autonomous program for red2 that looks like it is going to work

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/AUTONOMOUS_PROGRAM_9926.java
@@ -0,0 +1,115 @@
+package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
+
+/**
+ * Created by vijay on 12/14/2015.
+ */
+public class AUTONOMOUS_PROGRAM_9926 extends Telemetry9926 {
+
+
+    public AUTONOMOUS_PROGRAM_9926() {
+        // Copying same logic as PushBotAuto.java
+    }
+
+
+    int move_state = 0;
+//initial state is 0
+  ;
+
+
+    @Override
+    public void start() {
+        //
+        // Call the PushBotHardware (super/base class) start method.
+        //
+        super.start();
+    }
+
+@Override public void loop()
+        {
+        switch (move_state)
+        {
+        case 0:
+                /* might need to reset motor */
+        move_state++;
+            //moves to next state
+        break;
+
+        case 1:
+        if (getRuntime() > 1) {
+            MoveRobot(.5, .5);
+            //if time is greater than one second then move forwards
+            move_state++;
+            break;
+        }
+        case 2:
+            if (getRuntime() > 3) {
+            MoveRobot(0, .0);
+            //if time is greater than 3 second stop for one second
+            move_state++;
+        }
+        case 3:
+        if (getRuntime() > 4) {
+            MoveRobot(0, .5);
+            //if time is greater than 4 seconds point turn towards rescue zone
+            move_state++;
+            break;
+        }
+        case 4:
+        if (getRuntime() > 9) {
+            MoveRobot(0, .0);
+            //if time is greater than nine seconds stop for one second
+            move_state++;
+            }
+        case 5:
+            if (getRuntime() > 10) {
+                MoveRobot(.7, .7);
+                //if time is greater than 10 seconds move forwards for 5 seconds
+                move_state++;
+                break;
+            }
+        case 6:
+            if (getRuntime() > 15) {
+                MoveRobot(0, 0);
+                //if time is greater then 15 seconds stop for one second
+                move_state++;
+                break;
+            }
+        case 7:
+            if (getRuntime() > 16) {
+                MoveRobot(.4, .4);
+                //if time is greater than 16 seconds move forwards for 3 seconds
+                move_state++;
+                break;
+            }
+        case 8:
+            if (getRuntime() > 19) {
+                MoveRobot(0, 0);
+                //if time is greater than 19 seconds stop the autonomous program
+                move_state++;
+                break;
+            }
+            break;
+
+            default:
+                break;
+        }
+
+            UpdateTelemetry();
+            telemetry.addData("11", "State: " + move_state);
+            telemetry.addData("12", "Time: " + getRuntime());
+            //Telemetry data includes state number and time
+        }
+
+    @Override
+    public void stop() {
+//stops program
+    }
+
+
+
+}
+
+
+
+
+
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -67,12 +67,14 @@ public class FtcOpModeRegister implements OpModeRegister {
       manager.register("NullOp", NullOp.class);
       manager.register("Control_1", Control_1.class);
       manager.register("2 Gamepads", TwoGamepads.class);
+      manager.register("Autonomous_RED_2",AUTONOMOUS_PROGRAM_9926.class);
 
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
       manager.register("K9TeleOp", K9TeleOp.class);
       manager.register("K9Line", K9Line.class);
 //    manager.register ("PushBotAuto", PushBotAuto.class);
       manager.register ("PushBotManual", PushBotManual.class);
+
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
