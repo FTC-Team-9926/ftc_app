c6dd0fee59cf3a37d1a71aeec091f8f5ac631911
vijay <rudraraju.vijay5@gmail.com>
2015-12-15 21:27:52 -0500

    Made the AutoRed1.java program to go for the floor goal and push the debris from the front of the mountain

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/AutoRed1.java
@@ -0,0 +1,139 @@
+package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by vijay on 12/14/2015.
+ */
+public class AutoRed1 extends Telemetry9926 {
+
+
+
+    public AutoRed1() {
+        // Copying same logic as PushBotAuto.java
+    }
+
+
+    int move_state = 0;
+    double TimeNow = getRuntime();
+//initial state is 0
+
+
+    @Override
+    public void start() {
+        //
+        // Call the PushBotHardware (super/base class) start method.
+        //
+        Motor2.setDirection(DcMotor.Direction.REVERSE);
+        super.start();
+    }
+
+    @Override public void loop()
+    {
+
+        switch (move_state)
+        {
+            case 0:
+                /* might need to reset motor */
+                move_state++;
+                TimeNow = getRuntime();
+                //moves to next state
+                break;
+
+            case 1:
+                if (getRuntime() - TimeNow > 1) {
+                    MoveRobot(.5, .5);
+                    //if time is greater than one second then move forwards for 3 seconds
+                    move_state++;
+                    TimeNow = getRuntime();
+                    break;
+                }
+            case 2:
+                if (getRuntime() - TimeNow > .5) {
+                    MoveRobot(0, 0);
+                    //if time is greater than 4 second stop for one second
+                    move_state++;
+                    TimeNow = getRuntime();
+                    break;
+                }
+            case 3:
+
+                if (getRuntime() - TimeNow > 1) {
+                    MoveRobot(.375, 0);
+                    //if time is greater than 5 seconds point turn towards rescue zone
+                    move_state++;
+                    TimeNow = getRuntime();
+                    break;
+                }
+            case 4:
+                if (getRuntime() - TimeNow > 3) {
+                    MoveRobot(0, 0);
+                    //if time is greater than nine seconds stop for one second
+                    move_state++;
+                    TimeNow = getRuntime();
+                    break;
+                }
+            case 5:
+                if (getRuntime() - TimeNow > 1) {
+                    MoveRobot(.6, .6);
+                    //if time is greater than nine seconds move forwards for 5 seconds
+                    move_state++;
+                    TimeNow = getRuntime();
+                    break;
+                }
+            case 6:
+                if (getRuntime() - TimeNow > 3) {
+                    MoveRobot(0, 0);
+                    //if time is greater than ten seconds stop for one second
+                    move_state++;
+                    TimeNow = getRuntime();
+                    break;
+                }
+/*            case 7:
+                if (getRuntime() > 13) {
+                    MoveRobot(.5, 0);
+                    //if time is greater than ten seconds point turn for 3 seconds
+                    move_state++;
+                    break;
+                }
+            case 8:
+                if (getRuntime() > 13) {
+                    MoveRobot(0, 0);
+                    //if time is greater than 12 seconds stop for one second
+                    move_state++;
+                    break;
+                }
+            case 9:
+                if (getRuntime() > 14) {
+                    MoveRobot(.4, .4);
+                    //if time is greater than 13 seconds move forwards for 2 seconds
+                    move_state++;
+                    break;
+                }
+            case 10:
+                if (getRuntime() > 16.5) {
+                    MoveRobot(0, 0);
+                    //if time is greater than 15 seconds stop
+                    move_state++;
+                    break;
+                }
+                break;
+*/
+            default:
+                break;
+        }
+
+        UpdateTelemetry();
+        telemetry.addData("11", "State: " + move_state);
+        telemetry.addData("12", "Time: " + getRuntime());
+        //Telemetry data includes state number and time
+    }
+
+    @Override
+    public void stop() {
+//stops program
+    }
+
+
+
+}
