213561e8b1b7cdb850417408861773133f5c3f59
Nickstreme <nicobravo1@hotmail.com>
2015-12-18 18:18:25 -0500

    Removed any point before the program where the servo moves

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/AutoBlue1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/AutoBlue1.java
@@ -28,7 +28,6 @@ public class AutoBlue1 extends Telemetry9926{
         super.start();
         // sets Motor2's direction to go backwards, since the motor is flipped
         Motor2.setDirection(DcMotor.Direction.REVERSE);
-        Set_Servo2_position(0.5);
     }
 
     @Override public void loop()
